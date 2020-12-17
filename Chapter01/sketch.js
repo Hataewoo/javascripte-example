66e364ef68f02c2e9cd5e875f28b20199c4831b3
hataewoo <teawoo163@gmail.com>
2020-12-17 19:44:19 +0900

    ghj

---
+++ Chapter01/sketch.js
@@ -0,0 +1,12 @@
+
+    function setup()
+    {
+        createCanvas(800,600);
+        background(96,96,96);
+    }
+
+    function draw()
+    {
+       stroke(34,54,65);
+       line(pmouseX,pmouseY,mouseX,mouseY);
+    }
